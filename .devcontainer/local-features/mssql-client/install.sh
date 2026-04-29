615da6ecd9e033c6b33bb0fca1b03683b702efb5
Tim Nolte <tnolte@nbe-inc.com>
2026-04-29 13:06:19 -0400

    fix(MSSQL): Fixes bad MSSQL extension install script
    - Fixes and issue where the MSSQL Server extension support wasn't being installed because of an invalid conditional.

--- .devcontainer/local-features/mssql-client/install.sh
+++ .devcontainer/local-features/mssql-client/install.sh
@@ -6,17 +6,13 @@ export DEBIAN_FRONTEND=noninteractive
 PHP_VERSION=$(php -v | head -n 1 | cut -d " " -f 2);
 
 # Install the MSSQl Server client.
-if [ ! -f /usr/bin/mysql ]; then
+if [ -d /opt/mssql-tools18 ]; then
 	echo "Installing MSSQl Server client..."
 	sudo apt-get update -y --no-install-recommends
 	sudo apt-get install -y --no-install-recommends unixodbc-dev libgssapi-krb5-2;
-	sudo pickle install sqlsrv;
-	sudo pickle install pdo_sqlsrv;
-	sudo su;
-	printf "; priority=20\nextension=sqlsrv.so\n" > "/etc/php/$PHP_VERSION/mods-available/sqlsrv.ini";
-	printf "; priority=30\nextension=pdo_sqlsrv.so\n" > "/etc/php/$PHP_VERSION/mods-available/pdo_sqlsrv.ini";
-	exit;
-	sudo docker-php-ext-enable sqlsrv pdo_sqlsrv;
+	sudo pecl install sqlsrv pdo_sqlsrv;
+	sudo echo extension=sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/00_sqlsrv.ini
+	sudo echo extension=pdo_sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/10_pdo_sqlsrv.ini
 fi
 
 echo "Done!"
